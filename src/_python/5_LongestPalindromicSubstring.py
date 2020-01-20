bdcc51b4d714d8662c10e952f6248bba6c53f0e1
weiguozhao <aszhaoweiguo@hotmail.com>
2020-01-20 08:58:07 +0800

    update problem 5 longestPalindrome

--- src/_python/5_LongestPalindromicSubstring.py
+++ src/_python/5_LongestPalindromicSubstring.py
@@ -45,8 +45,59 @@ class Solution:
 
         return longest_palindrome
 
+    def longestPalindromeDynamicProgram(self, s: str) -> str:
+        """
+        time： O(n^2)
+        space: O(n^2)
+        :param s:
+        :return:
+        """
+        length = len(s)
+        if length <= 1:
+            return s
+
+        longest_palindrome = s[0]
+        is_palindrome = [[False for _ in range(length)] for _ in range(length)]
+        for i in range(length):
+            is_palindrome[i][i] = True
+            if i > 0 and s[i - 1] == s[i]:
+                is_palindrome[i - 1][i] = True
+                longest_palindrome = s[i - 1: i + 1]
+
+        for i in range(length - 1, -1, -1):
+            for j in range(i + 1, length):
+                if s[i] == s[j] and is_palindrome[i + 1][j - 1]:
+                    is_palindrome[i][j] = True
+                    if len(longest_palindrome) < j - i + 1:
+                        longest_palindrome = s[i: j + 1]
+
+        return longest_palindrome
+
+    def longestPalindromeDynamicProgramOptimizeSpace(self, s: str) -> str:
+        """
+        time： O(n^2)
+        space: O(n)
+        :param s:
+        :return:
+        """
+        length = len(s)
+        longest_palindrome = ""
+        is_palindrome = [False for _ in range(length)]
+
+        # is_palindrome[j] = is_palindrome[j-1] and s[i] == s[j]
+        # 第i行的结果只有第i+1行的结果决定
+        for i in range(length - 1, -1, -1):
+            for j in range(length - 1, i - 1, -1):
+                # 将单个字符、两个字符相等的情况放到这里一起判断
+                is_palindrome[j] = s[i] == s[j] and (j - i < 3 or is_palindrome[j - 1])
+                if len(longest_palindrome) < j - i + 1:
+                    longest_palindrome = s[i: j + 1]
+
+        return longest_palindrome
+
 
 if __name__ == '__main__':
-    s = "cbbd"
-    res = Solution().longestPalindrome(s)
-    print(res)
+    s = "abcda"
+    res1 = Solution().longestPalindrome(s)
+    res2 = Solution().longestPalindromeDynamicProgram(s)
+    print(res1, res2)
