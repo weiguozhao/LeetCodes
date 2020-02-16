130354d02b1111bb09e36945f3e780fdd9be35dc
weiguozhao <aszhaoweiguo@hotmail.com>
2020-02-16 10:05:44 +0800

    update problem 114

--- src/_python/hot100/114_FlattenBinaryTreetoLinkedList.py
+++ src/_python/hot100/114_FlattenBinaryTreetoLinkedList.py
@@ -37,6 +37,9 @@ class Solution:
               6
     """
 
+    def __init__(self):
+        self.pre = None
+
     def flatten(self, root: TreeNode) -> None:
         """
         Do not return anything, modify root in-place instead.
@@ -47,34 +50,45 @@ class Solution:
             if root.left is None:
                 root = root.right
             else:
-                # 左子树不为空
-                pre = root.left
-                # 找到左子树的最右下节点
-                while pre.right is not None:
-                    pre = pre.right
-                # 原来的右子树挂到左子树最右下
-                pre.right = root.right
-                # 原来的左子树挂到根的右子树
+                # 先将右子树摘下来
+                right = root.right
+                # 将左子树放到右子树的位置
                 root.right = root.left
                 # 原来的左子树置为空
                 root.left = None
-                # 往右下走一步
+                # 用一个临时变量去访问原来左子树的最右下的节点
+                pre = root.right
+                while pre.right is not None:
+                    pre = pre.right
+                # 原来的右子树挂到原来左子树的最右下节点上
+                pre.right = right
+                # 从根往下走步
                 root = root.right
 
-    def _postorder_travel_(self, root: TreeNode) -> None:
+    def _postorder_travel_recurrent_(self, root: TreeNode) -> None:
         """
-        后序遍历改版
+        后序遍历改版, 遍历顺序是右子树->左子树->根节点
         https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--26/
         """
+        if root is None:
+            return
+        self._postorder_travel_recurrent_(root.right)
+        self._postorder_travel_recurrent_(root.left)
+        root.right = self.pre
+        root.left = None
+        self.pre = root
+
+    def _postorder_travel_foreach_(self, root: TreeNode) -> None:
         to_visit = []
         curr, pre = root, None
         while curr is not None or len(to_visit) > 0:
-
+            # 后序遍历先将所有的最右节点加入到stack中
             while curr is not None:
                 to_visit.append(curr)
                 curr = curr.right
-
+            # stack中取一个元素
             curr = to_visit[-1]
+            # 在不存在左子树或者右子树已经访问过的情况下, 访问根
             if curr.left is None or curr.left == pre:
                 to_visit.pop()
 
@@ -83,7 +97,7 @@ class Solution:
 
                 pre = curr
                 curr = None
-
+            # 访问左子树
             else:
                 curr = curr.left
 
@@ -96,7 +110,7 @@ if __name__ == '__main__':
     root.left.right = TreeNode(4)
     root.right.right = TreeNode(6)
 
-    Solution().flatten(root)
+    Solution()._postorder_travel_foreach_(root)
     while root:
         print(root.val, end=" ")
         root = root.right
